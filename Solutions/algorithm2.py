de18f1797cb7f3d4b0992ee9d66c63f175e646cc
Aiden Clark <70608824+Aidenclark@users.noreply.github.com>
2023-06-13 14:21:52 -0500

    Update algorithm2.py

--- Solutions/algorithm2.py
+++ Solutions/algorithm2.py
@@ -1,1 +1,18 @@
+def count_unique_numbers(numbers):
+    unique_numbers = set()
+    duplicates = set()
+    
+    for num in numbers:
+        if num not in duplicates:
+            if num in unique_numbers:
+                unique_numbers.remove(num)
+                duplicates.add(num)
+            else:
+                unique_numbers.add(num)
+    
+    return len(unique_numbers)
 
+# Example usage
+numbers = [1, 2, 3, 4, 2, 3, 4, 5]
+result = count_unique_numbers(numbers)
+print(result)
